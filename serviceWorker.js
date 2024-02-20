a08bf7cc9cc87069fa12e748c6ea7f4c1178bab7
MNCC-Admin <mncc.dubaichurch@gmail.com>
2024-02-20 09:57:21 +0400

    nc

--- serviceWorker.js
+++ serviceWorker.js
@@ -20,11 +20,24 @@ var cacheFiles = [
   //  );
 //});
 
+// self.addEventListener('fetch ', function (e) {
+   // e.respondWith(
+      //  caches.match(e.request).then(function (r) {
+      //      console.log('[Service Worker] Caching all the files...'
+      //      + e.request.url);
+    //    })
+  //  );
+//});
+
 self.addEventListener('fetch ', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (r) {
-            console.log('[Service Worker] Caching all the files...'
-            + e.request.url);
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
     );
 });
